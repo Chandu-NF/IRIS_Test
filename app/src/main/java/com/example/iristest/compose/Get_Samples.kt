521d3150b0a018645d06019b0cb6037391284194
Chandu Nadella <Ranjith.Sarikonda@genusplc.com>
2024-08-01 16:58:05 +0530

    Get_Samples2 Commit

--- app/src/main/java/com/example/iristest/compose/Get_Samples.kt
+++ app/src/main/java/com/example/iristest/compose/Get_Samples.kt
@@ -2,9 +2,10 @@ package com.example.iristest.compose
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,16 +14,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -34,10 +35,9 @@ import androidx.compose.ui.unit.sp
 import com.example.iristest.R
 
 
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
 @Composable
 fun Get_Samples() {
-    var presses by remember { mutableIntStateOf(0) }
+    var presses by remember { mutableStateOf(false) }
 
     Scaffold(
         topBar = {
@@ -92,12 +92,18 @@ fun Get_Samples() {
         ) {
             Button(
                 modifier = Modifier
-                    .padding(32.dp),
+                    .background(colorResource(id = R.color.box_bg))
+                    .border(
+                        width = 2.dp,
+                        color = if (presses) Color.White else Color.Black
+                    ),
+                enabled = true,
                 shape = RectangleShape,
+                onClick = { presses != presses },
                 colors = ButtonDefaults.buttonColors(
-                    containerColor = colorResource(id = R.color.bg_black)
-                ),
-                onClick = { /* Do something! */ }
+                    if (presses) Color.White
+                    else Color.Black
+                )
             ) {
                 Column {
                     Row {
@@ -105,14 +111,19 @@ fun Get_Samples() {
                             text = "1",
                             fontSize = 32.sp
                         )
-                        Image(
-                            painter = painterResource(id = R.drawable.vector),
-                            contentDescription = "Get My Samples",
+                        Box(
                             modifier = Modifier
-                                .fillMaxWidth(0.2f)
-                                .fillMaxHeight(0.3f)
-                                .padding(16.dp)
-                        )
+                                .background(colorResource(id = R.color.box_bg))
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.vector),
+                                contentDescription = "Get My Samples",
+                                modifier = Modifier
+                                    .fillMaxWidth(0.2f)
+                                    .fillMaxHeight(0.3f)
+                                    .padding(40.dp)
+                            )
+                        }
                     }
                     Text(
                         modifier = Modifier
@@ -125,7 +136,7 @@ fun Get_Samples() {
             }
             Button(
                 modifier = Modifier
-                    .padding(32.dp),
+                    .background(colorResource(id = R.color.box_bg)),
                 shape = RectangleShape,
                 colors = ButtonDefaults.buttonColors(
                     containerColor = colorResource(id = R.color.bg_black)
@@ -133,7 +144,10 @@ fun Get_Samples() {
                 onClick = { /* Do something! */ }
             )
             {
-                Column {
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
                     Row {
                         Text(
                             modifier = Modifier
@@ -141,14 +155,21 @@ fun Get_Samples() {
                             text = "2",
                             fontSize = 32.sp,
                         )
-                        Image(
-                            painter = painterResource(id = R.drawable.vector_1),
-                            contentDescription = "Scan Samples",
+                        Box(
                             modifier = Modifier
-                                .fillMaxWidth(0.3f)
-                                .fillMaxHeight(0.3f)
-                                .padding(18.dp)
-                        )
+                                .background(colorResource(id = R.color.box_bg)),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.vector_1),
+                                contentDescription = "Scan Samples",
+                                modifier = Modifier
+                                    .fillMaxWidth(0.3f)
+                                    .fillMaxHeight(0.3f)
+                                    .padding(40.dp),
+                                Alignment.Center
+                            )
+                        }
                     }
                     Text(
                         modifier = Modifier
@@ -159,9 +180,7 @@ fun Get_Samples() {
                     )
                 }
             }
-
         }
-
     }
 }
 
