1ddf8892b565bb073dca3f847ee8842bcc07e8f7
Chandu Nadella <Ranjith.Sarikonda@genusplc.com>
2024-08-02 17:45:51 +0530

    Get_Samples3 Commit

--- app/src/main/java/com/example/iristest/compose/Get_Samples.kt
+++ app/src/main/java/com/example/iristest/compose/Get_Samples.kt
@@ -1,165 +1,6 @@
 package com.example.iristest.compose
 
 
-//@OptIn(ExperimentalMaterial3Api::class)
-//@Composable
-//fun Get_Samples() {
-//    var presses by remember { mutableStateOf(false) }
-//
-//    Scaffold(
-//        topBar = {
-//            Row(
-//                modifier = Modifier
-//                    .background(colorResource(id = R.color.bg_black))
-//                    .fillMaxWidth(),
-//                verticalAlignment = Alignment.CenterVertically,
-//                horizontalArrangement = Arrangement.SpaceBetween
-//
-//            ) {
-//                Button(
-//                    modifier = Modifier
-//                        .padding(32.dp)
-//                        .wrapContentSize(),
-//                    shape = RectangleShape,
-//                    colors = ButtonDefaults.buttonColors(
-//                        containerColor = colorResource(id = R.color.box_bg)
-//                    ),
-//                    onClick = { /* Do something! */ }
-//                ) {
-//                    Text(
-//                        "Back",
-//                        fontSize = 24.sp
-//                    )
-//                }
-//                Button(
-//                    modifier = Modifier
-//                        .padding(32.dp)
-//                        .wrapContentSize(),
-//                    shape = RectangleShape,
-//                    colors = ButtonDefaults.buttonColors(
-//                        containerColor = colorResource(id = R.color.box_bg)
-//                    ),
-//                    onClick = { /* Do something! */ }
-//                ) {
-//                    Text(
-//                        "Menu",
-//                        fontSize = 24.sp
-//                    )
-//                }
-//            }
-//        }
-//    ) { innerPadding ->
-//        Row(
-//            modifier = Modifier
-//                .padding(innerPadding)
-//                .fillMaxSize()
-//                .background(colorResource(id = R.color.bg_black)),
-//            verticalAlignment = Alignment.CenterVertically,
-//            horizontalArrangement = Arrangement.Center
-//        ) {
-//            Button(
-//                modifier = Modifier
-//                    .background(colorResource(id = R.color.box_bg))
-//                    .border(
-//                        width = 2.dp,
-//                        color = if (presses) Color.White else Color.Black
-//                    ),
-//                enabled = true,
-//                shape = RectangleShape,
-//                onClick = { presses != presses },
-//                colors = ButtonDefaults.buttonColors(
-//                    if (presses) Color.White
-//                    else Color.Black
-//                )
-//            ) {
-//                Column {
-//                    Row {
-//                        Text(
-//                            text = "1",
-//                            fontSize = 32.sp
-//                        )
-//                        Box(
-//                            modifier = Modifier
-//                                .background(colorResource(id = R.color.box_bg))
-//                        ) {
-//                            Image(
-//                                painter = painterResource(id = R.drawable.vector),
-//                                contentDescription = "Get My Samples",
-//                                modifier = Modifier
-//                                    .fillMaxWidth(0.2f)
-//                                    .fillMaxHeight(0.3f)
-//                                    .padding(40.dp)
-//                            )
-//                        }
-//                    }
-//                    Text(
-//                        modifier = Modifier
-//                            .padding(16.dp),
-//                        text = "Get My Samples",
-//                        fontSize = 32.sp,
-//                        textAlign = TextAlign.Center
-//                    )
-//                }
-//            }
-//            Button(
-//                modifier = Modifier
-//                    .background(colorResource(id = R.color.box_bg)),
-//                shape = RectangleShape,
-//                colors = ButtonDefaults.buttonColors(
-//                    containerColor = colorResource(id = R.color.bg_black)
-//                ),
-//                onClick = { /* Do something! */ }
-//            )
-//            {
-//                Column(
-//                    verticalArrangement = Arrangement.Center,
-//                    horizontalAlignment = Alignment.CenterHorizontally
-//                ) {
-//                    Row {
-//                        Text(
-//                            modifier = Modifier
-//                                .padding(18.dp),
-//                            text = "2",
-//                            fontSize = 32.sp,
-//                        )
-//                        Box(
-//                            modifier = Modifier
-//                                .background(colorResource(id = R.color.box_bg)),
-//                            contentAlignment = Alignment.Center
-//                        ) {
-//                            Image(
-//                                painter = painterResource(id = R.drawable.vector_1),
-//                                contentDescription = "Scan Samples",
-//                                modifier = Modifier
-//                                    .fillMaxWidth(0.3f)
-//                                    .fillMaxHeight(0.3f)
-//                                    .padding(40.dp),
-//                                Alignment.Center
-//                            )
-//                        }
-//                    }
-//                    Text(
-//                        modifier = Modifier
-//                            .padding(16.dp),
-//                        text = "Scan Samples",
-//                        fontSize = 32.sp,
-//                        textAlign = TextAlign.Center
-//                    )
-//                }
-//            }
-//        }
-//    }
-//}
-//
-//
-//
-//@Preview(device = Devices.TABLET)
-//@Composable
-//fun Get_Samples_Preview() {
-//    Get_Samples()
-//}
-
-
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -169,15 +10,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.displayCutoutPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -188,7 +28,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -198,54 +37,41 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.iristest.R
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.ui.draw.shadow
 
 enum class SampleOption {
     GetMySamples,
-    ScanSamples
+    ScanSamples,
+    Menu,
+    Back
 }
 
 
 @Composable
 fun GetSamplesScreen() {
     var selectedOption by remember { mutableStateOf(SampleOption.GetMySamples) }
+    var selectedButton by remember { mutableStateOf(SampleOption.Menu) }
 
     Scaffold(
         topBar = {
             Row(
                 modifier = Modifier
                     .background(colorResource(id = R.color.bg_black))
-                    .fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
+                    .fillMaxWidth()
+                    .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Button(
-                    modifier = Modifier
-                        .padding(32.dp)
-                        .wrapContentSize()
-                        .background(colorResource(id = R.color.box_bg)),
-                    onClick = { /* Handle back action */ }
-                ) {
-                    Text(
-                        "Back",
-                        fontSize = 24.sp
-                    )
-                }
-                Button(
-                    modifier = Modifier
-                        .padding(32.dp)
-                        .wrapContentSize()
-                        .background(colorResource(id = R.color.box_bg)),
-                    onClick = { /* Handle menu action */ }
-                ) {
-                    Text(
-                        "Menu",
-                        fontSize = 24.sp
-                    )
-                }
-            }
+            MenuBack(
+                painter = painterResource(id = R.drawable.frame_56779__1_),
+                isSelected = selectedButton == SampleOption.Back,
+                onClick = { selectedButton = SampleOption.Back }
+            )
+            MenuBack(
+                painter = painterResource(id = R.drawable.frame_56780),
+                isSelected = selectedButton == SampleOption.Menu,
+                onClick = { selectedButton = SampleOption.Menu }
+            )
+        }
         }
     ) { innerPadding ->
         Row(
@@ -278,6 +104,37 @@ fun GetSamplesScreen() {
 }
 
 @Composable
+fun MenuBack(
+    painter: Painter,
+    isSelected: Boolean,
+    onClick: () -> Unit,
+) {
+    Row(
+        modifier = Modifier
+            .background(
+                if (isSelected) colorResource(id = R.color.box_bg)
+                else Color.Transparent
+            )
+            .clickable { onClick() }
+            .border(
+                width = 2.dp,
+                color = if (isSelected) colorResource(id = R.color.box_bg)
+                else Color.Transparent,
+                shape = RoundedCornerShape(32.dp)
+            ),
+    ) {
+        Image(
+            painter = painter,
+            contentDescription = null,
+            modifier = Modifier
+                .size(96.dp)
+
+        )
+    }
+}
+
+
+@Composable
 fun SampleButton(
     label: String,
     painter: Painter,
@@ -297,8 +154,10 @@ fun SampleButton(
         ) {
             Box(
                 modifier = Modifier
-                    .background(if (isSelected) colorResource(id = R.color.purple_200)
-                    else Color.Transparent, shape = CircleShape)
+                    .background(
+                        if (isSelected) colorResource(id = R.color.purple_200)
+                        else Color.Transparent, shape = CircleShape
+                    )
                     .size(48.dp)
                     .border(
                         width = 1.dp,
@@ -308,8 +167,9 @@ fun SampleButton(
                     ),
                 contentAlignment = Alignment.Center
             ) {
-                Text(modifier = Modifier
-                    .padding(8.dp),
+                Text(
+                    modifier = Modifier
+                        .padding(8.dp),
                     text = "$index",
                     fontSize = 24.sp,
                     color = Color.White
@@ -344,7 +204,7 @@ fun SampleButton(
             color = Color.White,
             modifier = Modifier
                 .padding(16.dp)
-                .align(Alignment.CenterHorizontally)
+                .align(Alignment.End)
         )
     }
 }
