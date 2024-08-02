48a9f56e9b9eded812a51998bbdc754ee39fe2a0
Chandu Nadella <Ranjith.Sarikonda@genusplc.com>
2024-08-02 21:04:23 +0530

    Get_Samples4 Commit

--- app/src/main/java/com/example/iristest/compose/Get_Samples.kt
+++ app/src/main/java/com/example/iristest/compose/Get_Samples.kt
@@ -10,12 +10,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
@@ -57,21 +57,21 @@ fun GetSamplesScreen() {
                 modifier = Modifier
                     .background(colorResource(id = R.color.bg_black))
                     .fillMaxWidth()
-                    .padding(16.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween
+                    .padding(4.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
             ) {
-            MenuBack(
-                painter = painterResource(id = R.drawable.frame_56779__1_),
-                isSelected = selectedButton == SampleOption.Back,
-                onClick = { selectedButton = SampleOption.Back }
-            )
-            MenuBack(
-                painter = painterResource(id = R.drawable.frame_56780),
-                isSelected = selectedButton == SampleOption.Menu,
-                onClick = { selectedButton = SampleOption.Menu }
-            )
-        }
+                MenuBack(
+                    painter = painterResource(id = R.drawable.frame_56779__1_),
+                    isSelected = selectedButton == SampleOption.Back,
+                    onClick = { selectedButton = SampleOption.Back }
+                )
+                MenuBack(
+                    painter = painterResource(id = R.drawable.frame_56780),
+                    isSelected = selectedButton == SampleOption.Menu,
+                    onClick = { selectedButton = SampleOption.Menu }
+                )
+            }
         }
     ) { innerPadding ->
         Row(
@@ -109,25 +109,24 @@ fun MenuBack(
     isSelected: Boolean,
     onClick: () -> Unit,
 ) {
-    Row(
+    Box(
         modifier = Modifier
             .background(
                 if (isSelected) colorResource(id = R.color.box_bg)
-                else Color.Transparent
-            )
+                else Color.Transparent)
             .clickable { onClick() }
             .border(
                 width = 2.dp,
                 color = if (isSelected) colorResource(id = R.color.box_bg)
                 else Color.Transparent,
-                shape = RoundedCornerShape(32.dp)
+
             ),
     ) {
         Image(
             painter = painter,
             contentDescription = null,
             modifier = Modifier
-                .size(96.dp)
+                .size(150.dp)
 
         )
     }
@@ -146,10 +145,10 @@ fun SampleButton(
         modifier = Modifier
             .clickable { onClick() }
             .background(colorResource(id = R.color.bg_black))
-            .padding(16.dp)
-            .wrapContentSize()
+            .padding(32.dp)
     ) {
-        Row(
+        Row(modifier = Modifier
+            .padding(32.dp),
             horizontalArrangement = Arrangement.Center
         ) {
             Box(
@@ -158,7 +157,7 @@ fun SampleButton(
                         if (isSelected) colorResource(id = R.color.purple_200)
                         else Color.Transparent, shape = CircleShape
                     )
-                    .size(48.dp)
+                    .size(56.dp)
                     .border(
                         width = 1.dp,
                         color = if (isSelected) colorResource(id = R.color.purple_200)
@@ -175,7 +174,7 @@ fun SampleButton(
                     color = Color.White
                 )
             }
-            Spacer(modifier = Modifier.width(8.dp))
+            Spacer(modifier = Modifier.width(16.dp))
             Box(
                 modifier = Modifier
                     .background(colorResource(id = R.color.box_bg), RoundedCornerShape(32.dp))
@@ -185,7 +184,7 @@ fun SampleButton(
                         else Color.Transparent,
                         shape = RoundedCornerShape(32.dp)
                     )
-                    .size(150.dp),
+                    .size(200.dp),
                 contentAlignment = Alignment.Center
             ) {
                 Image(
@@ -204,7 +203,7 @@ fun SampleButton(
             color = Color.White,
             modifier = Modifier
                 .padding(16.dp)
-                .align(Alignment.End)
+                .align(Alignment.CenterHorizontally)
         )
     }
 }
